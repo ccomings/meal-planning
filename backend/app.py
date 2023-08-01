ca6fcfa0afaa481ba72750bd1a50cf05f25cedbf
Chris Comings <ccomings@gmail.com>
2023-08-01 14:56:31 -0700

    add docker

--- backend/app.py
+++ backend/app.py
@@ -3,5 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def homepage():
     return "<p>Hello, World!</p>"
+
+@app.route("/login", methods=["POST"])
+def login():
+    return 'login'
+
+@app.route("/logout", methods=['POST'])
+def logout():
+    return 'logout'
+
+@app.route('/register', methods=["POST"])
+def register():
+    return 'register'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
